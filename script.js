e0b0cabbcdf66f9ef73132cc0319156a427949b7
SWAGGA TH4 K1NG <97411896+SWAGGATH4K1NG@users.noreply.github.com>
2025-02-19 18:48:39 +0000

    update
    add the js scripts to new website, and a terminal and some dark/white modes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-document.addEventListener("DOMContentLoaded",function() { 
+document.addEventListener("DOMContentLoaded",function() {
+    document.querySelectorAll('.fade-in').forEach((element) => {
+        element.classList.add('show');
     let titulo = document.getElementById("titulo");
     let texto = "Olá, eu sou o Luís Moreira";
     let index = 0;
+    
 
         function escreverTexto(){
             if (index < texto.length) {
@@ -14,3 +17,31 @@ document.addEventListener("DOMContentLoaded",function() {
         titulo.innerHTML = ""; // usado para limpar texto antes de iniciar
         escreverTexto();
     });
+});
+
+document.getElementById("console-input").addEventListener("keypress", function(event){
+    if (event.key === "Enter") {
+        let command = this.value.toLocaleLowerCase();
+        let response = "> Command not founded!";
+
+        if (command ==="!about me") response = "> Junior programming student and tech enthusiast.";
+        if (command ==="!github") response = "> My github https://github.com/SWAGGATH4K1NG";
+        if (command ==="!help") response = "> Use the following commands [!about me, ! github]"
+
+        let consoleText = document.createElement("p");
+        consoleText.textContent = `> ${command}`;
+        document.getElementById("terminal").appendChild(consoleText);
+
+        let consoleResponse = document.createElement("p");
+        consoleResponse.textContent = response;
+        document.getElementById("terminal").appendChild(consoleResponse);
+
+        this.value = ""; // Cleans the input
+
+    }
+
+});
+
+document.getElementById("toggle-theme").addEventListener("click", function(){
+    document.body.classList.toggle("light-mode");
+});
