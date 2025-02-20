01f3faf31a16f2230198b4518f5452e423988264
SWAGGA TH4 K1NG <97411896+SWAGGATH4K1NG@users.noreply.github.com>
2025-02-20 18:05:08 +0000

    upload
    another upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded",function() {
     document.querySelectorAll('.fade-in').forEach((element) => {
         element.classList.add('show');
     let titulo = document.getElementById("titulo");
-    let texto = "Olá, eu sou o Luís Moreira";
+    let texto = "Hello, i am Luís Moreira";
     let index = 0;
     
 
@@ -14,17 +14,17 @@ document.addEventListener("DOMContentLoaded",function() {
             }
         }
 
-        titulo.innerHTML = ""; // usado para limpar texto antes de iniciar
+        titulo.innerHTML = ""; // USED TO CLEAR THE TEXT BEFORE STARTS
         escreverTexto();
     });
 });
-
+// THIS IS FOR THE TERMINAL
 document.getElementById("console-input").addEventListener("keypress", function(event){
     if (event.key === "Enter") {
         let command = this.value.toLocaleLowerCase();
         let response = "> Command not founded!";
 
-        if (command ==="!about me") response = "> Junior programming student and tech enthusiast.";
+        if (command ==="!aboutme") response = "> Junior programming student and tech enthusiast.";
         if (command ==="!github") response = "> My github https://github.com/SWAGGATH4K1NG";
         if (command ==="!help") response = "> Use the following commands [!about me, !github]"
 
@@ -41,7 +41,46 @@ document.getElementById("console-input").addEventListener("keypress", function(e
     }
 
 });
+// THIS IS THE END OF THE TERMINAL
 
+
+
+
+// PLUGIN OF THE LIGHT AND DARK MODE
 document.getElementById("toggle-theme").addEventListener("click", function(){
     document.body.classList.toggle("light-mode");
 });
+
+document.getElementById("toggle-theme").addEventListener("click", function() {
+    document.body.classList.toggle("light-mode");
+    this.classList.toggle("active");
+});
+
+document.getElementById("toggle-theme").addEventListener("click", function() {
+    document.body.classList.toggle("light-mode");
+
+    // Change the lamp icon
+    let lampIcon = this.querySelector("i");
+    if (document.body.classList.contains("light-mode")) {
+        lampIcon.classList.remove("fa-lightbulb");
+        lampIcon.classList.add("fa-moon"); // MOON ICON CHANGE TO LIGHT
+    } else {
+        lampIcon.classList.remove("fa-moon");
+        lampIcon.classList.add("fa-lightbulb"); // ICON CHANGE TO DARK
+    }
+});
+
+
+// PLUGIN OF THE LIGHT AND DARK MODE
+
+
+
+
+// PLUGIN TO SAY MESSAGE SEND
+document.getElementById("contact-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    alert("Mensagem Enviada!");
+ });
+
+ //PLUGIN TO SAY MESSAGE SENDED
+
